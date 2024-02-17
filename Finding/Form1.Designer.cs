ea4088a7fd4d738dcd047655665dc15e39939409
Dien Le <dien.lephuoc@gameloft.com>
2024-02-17 15:55:35 +0700

    u

---
+++ Finding/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Finding
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(-1, -2);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1913, 982);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(-1, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 28);
+            comboBox1.TabIndex = 4;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1924, 983);
+            Controls.Add(comboBox1);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "...";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private FlowLayoutPanel flowLayoutPanel1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Finding/Form1.cs
@@ -0,0 +1,209 @@
+namespace Finding
+{
+    public enum Pattern
+    {
+        Pattern1,//giam :gia giam tu 65 ve 500
+        Pattern2,//tang :gia tu 500 tang len,
+        Pattern3,//tang :gia vuot 500
+        
+        
+ 
+
+
+    }
+    public partial class Form1 : Form
+    {
+
+
+
+        // private Dictionary<Pattern, byte[]> imageDictionary = new Dictionary<Pattern, byte[]>();
+        private Dictionary<Pattern, List<byte[]>> imageDictionary = new Dictionary<Pattern, List<byte[]>>();
+        private ToolTip comboBoxToolTip = new ToolTip();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ShowImage();
+        }
+        private void Init()
+        {
+            LoadPatternImages();
+            //ShowImage();
+            PopulateComboBox();
+            SetComboBoxToolTip();
+        }
+        private void LoadPatternImages()
+        {
+            foreach (Pattern pattern in Enum.GetValues(typeof(Pattern)))
+            {
+                List<byte[]> patternImages = new List<byte[]>();
+                int imageIndex = 0;
+
+                while (true)
+                {
+                    string imageName = $"{pattern}{(imageIndex == 0 ? "" : $"_{imageIndex}")}.jpg";
+                    byte[] imageData;
+
+                    try
+                    {
+                        imageData = File.ReadAllBytes(imageName);
+                        patternImages.Add(imageData);
+                        imageIndex++;
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error loading image for {pattern}: {ex.Message}");
+                        break;
+                    }
+                }
+
+                if (patternImages.Count > 0)
+                {
+                    imageDictionary[pattern] = patternImages;
+                }
+            }
+        }
+
+
+        private void ShowImage()
+        {
+
+
+            int x = 10;
+            int y = 20;
+            int pictureBoxWidth = 10;
+            int pictureBoxHeight = 10;
+
+            foreach (var kvp in imageDictionary)
+            {
+                Pattern pattern = kvp.Key;
+                List<byte[]> patternImages = kvp.Value;
+
+                foreach (byte[] imageData in patternImages)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Location = new Point(x, y);
+                    pictureBox.Size = new Size(pictureBoxWidth, pictureBoxHeight);
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox.Image = Image.FromStream(new MemoryStream(imageData));
+                    pictureBox.Click += (sender, e) =>
+                    {
+
+                        ShowLargeImage(pattern, imageData);
+                    };
+                    flowLayoutPanel1.Controls.Add(pictureBox);
+
+                    x += pictureBoxWidth + 5;
+                }
+            }
+
+        }
+        private void ShowImages(Pattern pattern)
+        {
+
+            flowLayoutPanel1.Controls.Clear();
+
+            if (imageDictionary.ContainsKey(pattern))
+            {
+                foreach (byte[] imageData in imageDictionary[pattern])
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Size = new Size(135, 135);
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox.Image = Image.FromStream(new MemoryStream(imageData));
+                    pictureBox.Click += (sender, e) =>
+                    {
+
+                        ShowLargeImage(pattern, imageData);
+                    };
+                    flowLayoutPanel1.Controls.Add(pictureBox);
+                }
+            }
+        }
+
+        private void ShowLargeImage(Pattern pattern, byte[] imageData)
+        {
+
+            Form imageForm = new Form();
+            imageForm.Text = pattern.ToString();
+            imageForm.StartPosition = FormStartPosition.CenterScreen;
+            imageForm.Size = new Size(900, 700);
+            PictureBox largePictureBox = new PictureBox();
+            largePictureBox.Dock = DockStyle.Fill;
+            largePictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            largePictureBox.Image = Image.FromStream(new MemoryStream(imageData));
+
+            imageForm.Controls.Add(largePictureBox);
+
+
+            largePictureBox.Click += (sender, e) =>
+            {
+                imageForm.Close();
+            };
+
+            imageForm.ShowDialog();
+        }
+        private void PopulateComboBox()
+        {
+            comboBox1.DataSource = Enum.GetValues(typeof(Pattern));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                Pattern selectedPattern = (Pattern)comboBox1.SelectedItem;
+                ShowImages(selectedPattern);
+            }
+        }
+        private void SetComboBoxToolTip()
+        {
+            comboBoxToolTip.ToolTipTitle = "Pattern Explanation";
+            comboBoxToolTip.SetToolTip(comboBox1, "");
+
+            comboBox1.MouseEnter += (sender, e) =>
+            {
+                //  if (comboBox1.SelectedItem != null)
+                {
+                    Pattern selectedPattern = (Pattern)comboBox1.SelectedItem;
+                    string tooltipText = GetPatternExplanation(selectedPattern);
+                    comboBoxToolTip.SetToolTip(comboBox1, tooltipText);
+                }
+            };
+        }
+        private string GetPatternExplanation(Pattern pattern)
+        {
+
+            switch (pattern)
+            {
+                case Pattern.Pattern1:
+                    return "Gia giam tu 65 ve 500";
+                case Pattern.Pattern2:
+                    return "Gia tang tu 500 di len";
+                case Pattern.Pattern3:
+                    return "Gia tang vuot 500";
+
+                
+              
+                default:
+                    return "No explanation available.";
+            }
+        }
+
+
+    }
+}
